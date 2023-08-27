36c509ca0d471f20f598024b26a2923c21e26c2a
hsyang011 <hsyang011@gmail.com>
2023-08-27 11:33:14 +0900

    2023-08-27 JSP 숙제 - 아이디 중복확인 완료

--- src/main/java/homework/regist/RegistDAO.java
+++ src/main/java/homework/regist/RegistDAO.java
@@ -36,4 +36,28 @@ public class RegistDAO extends JDBConnect {
 		return result;
 	}
 	
+	// 아이디가 존재하는지 검사
+	public boolean isExist(String id) {
+		// 동적 쿼리문 작성
+		String query = " SELECT id FROM regist_member WHERE id=? ";
+		try {
+			psmt = con.prepareStatement(query);
+			// 첫번째 인파라미터에 매개변수id를 대입
+			psmt.setString(1, id);
+			// 쿼리 실행
+			rs = psmt.executeQuery();
+			
+			// rs가 존재할경우 false 반환
+			if (rs.next()) {
+				return false; 
+			}
+		} catch (Exception e) {
+			System.out.println("아이디 중복확인 중 예외 발생");
+			e.printStackTrace();
+		}
+		
+		// if문에서 걸리지 않았다면 true 반환
+		return true;
+	}
+	
 }
